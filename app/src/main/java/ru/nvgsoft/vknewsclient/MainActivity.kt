aa6af9a47d85b49ee59c40ef36929530a4339485
Daniil Shcherbakov <Charel@bk.ru>
2024-12-25 11:22:21 +0300

    Edit PostCard

--- app/src/main/java/ru/nvgsoft/vknewsclient/MainActivity.kt
+++ app/src/main/java/ru/nvgsoft/vknewsclient/MainActivity.kt
@@ -2,11 +2,33 @@ package ru.nvgsoft.vknewsclient
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import ru.nvgsoft.vknewsclient.ui.theme.PostCard
+import ru.nvgsoft.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContent {
+            VkNewsClientTheme {
+                // A surface container using the 'background' color from the theme
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(8.dp),
+                ) {
+                    PostCard()
+                }
+            }
+        }
     }
 }
 
--- app/src/main/java/ru/nvgsoft/vknewsclient/ui/theme/PostCard.kt
+++ app/src/main/java/ru/nvgsoft/vknewsclient/ui/theme/PostCard.kt
@@ -2,9 +2,7 @@ package ru.nvgsoft.vknewsclient.ui.theme
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,8 +11,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.Card
@@ -26,113 +26,125 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.nvgsoft.vknewsclient.R
 
 @Composable
-fun postCard() {
-    Card(
-        modifier = Modifier.padding(8.dp),
-        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.background),
-        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
-        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
+fun PostCard() {
+    Card {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(8.dp)
+        ) {
+            PostHeader()
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = stringResource(R.string.template_text))
+            Spacer(modifier = Modifier.height(8.dp))
+            Image(
+                modifier = Modifier.fillMaxWidth(),
+                painter = painterResource(id = R.drawable.post_content_image),
+                contentScale = ContentScale.FillWidth,
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Statistic()
+        }
+    }
+}
+
+
+@Composable
+fun PostHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Column(modifier = Modifier
-            .padding(8.dp)) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ){
-                Image(
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .size(50.dp),
-                    painter = painterResource(id = R.drawable.post_comunity_thumbnail),
-                    contentDescription = null
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Column(modifier = Modifier.weight(1f)) {
-                    Text(
-                        text = "/dev/null",
-                        color = MaterialTheme.colorScheme.onPrimary)
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        text = "14:00",
-                        color = MaterialTheme.colorScheme.onSecondary)
-                }
-                Icon(
-                    imageVector = Icons.Rounded.MoreVert,
-                    contentDescription = ""
-                )
-            }
-            Text(text = "кабаныч когда узнал, что если сотрудникам не платить они начнут умирать от голода")
-//            Image(
-//                modifier = Modifier.fillMaxWidth(),
-//                alignment = Alignment.Center,
-//                painter = painterResource(id = R.drawable.post_content_image),
-//                contentDescription = ""
-//            )
-//            Row(modifier = Modifier
-//                .fillMaxWidth(),
-//                horizontalArrangement = Arrangement.SpaceBetween,
-//                verticalAlignment = Alignment.CenterVertically)
-//            {
-//                Row(verticalAlignment = Alignment.CenterVertically) {
-//                    Text(text = "120")
-//                    Image(
-//                        modifier = Modifier
-//                            .size(15.dp),
-//                        painter = painterResource(id = R.drawable.ic_views_count),
-//                        contentDescription = ""
-//                    )
-//                }
-//                Row(verticalAlignment = Alignment.CenterVertically) {
-//                    Text(text = "206")
-//                    Image(
-//                        modifier = Modifier
-//                            .size(15.dp),
-//                        painter = painterResource(id = R.drawable.ic_share),
-//                        contentDescription = ""
-//                    )
-//
-//                    Text(text = "11")
-//                    Image(
-//                        modifier = Modifier
-//                            .size(15.dp),
-//                        painter = painterResource(id = R.drawable.ic_comment),
-//                        contentDescription = ""
-//                    )
-//                    Text(text = "491")
-//                    Image(
-//                        modifier = Modifier
-//                            .size(15.dp),
-//                        painter = painterResource(id = R.drawable.ic_like),
-//                        contentDescription = ""
-//                    )
-//                }
-//
-//            }
+        Image(
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(50.dp),
+            painter = painterResource(id = R.drawable.post_comunity_thumbnail),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = "/dev/null",
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "14:00",
+                color = MaterialTheme.colorScheme.onSecondary
+            )
+        }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = ""
+        )
+    }
+}
+
+@Composable
+fun Statistic() {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Row(
+            Modifier.weight(1f)) {
+            IconWithText(iconResId = R.drawable.ic_views_count, text = "10")
+        }
+        Row(
+            Modifier.weight(1f),
+            horizontalArrangement = Arrangement.SpaceBetween) {
+            IconWithText(iconResId = R.drawable.ic_share, text = "22")
+            IconWithText(iconResId = R.drawable.ic_comment, text = "6")
+            IconWithText(iconResId = R.drawable.ic_like, text = "37")
         }
 
+
     }
+}
+
+
+@Composable
+fun IconWithText(
+    iconResId: Int,
+    text: String
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Icon(
+            painter = painterResource(id = iconResId),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSecondary
+        )
+
+    }
+
 }
 
 @Preview
 @Composable
-fun postCardDarkTheme(){
+fun postCardDarkTheme() {
     VkNewsClientTheme(darkTheme = true, dynamicColor = false) {
-        postCard()
+        PostCard()
     }
 }
 
 @Preview
 @Composable
-fun postCardLightTheme(){
+fun postCardLightTheme() {
     VkNewsClientTheme(darkTheme = false, dynamicColor = false) {
-        postCard()
+        PostCard()
     }
 }
