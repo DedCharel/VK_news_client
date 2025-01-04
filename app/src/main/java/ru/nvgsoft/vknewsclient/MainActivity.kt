0c93979f467354996de8de822f5f2e8374f4fc4d
Daniil Shcherbakov <Charel@bk.ru>
2025-01-04 10:24:00 +0300

    ModalNavigationDrawer and Scaffold

--- app/src/main/java/ru/nvgsoft/vknewsclient/MainActivity.kt
+++ app/src/main/java/ru/nvgsoft/vknewsclient/MainActivity.kt
@@ -4,13 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -33,65 +54,151 @@ class MainActivity : ComponentActivity() {
 //                    PostCard()
 //                }
 //            }
-            Test()
+            Test2()
         }
     }
 }
 
 @Composable
-private fun Test(){
+private fun Test() {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
-    ){
+    ) {
         Example3()
     }
 }
 
 @Composable
-private fun Example1(){
+private fun Example1() {
     OutlinedButton(onClick = { /*TODO*/ }, shape = RoundedCornerShape(4.dp)) {
         Text(text = "Hello world")
-    }   
+    }
 }
 
 @Composable
-private fun Example2(){
+private fun Example2() {
     TextField(
         value = "Value",
         onValueChange = {},
-        label = { Text(text = "Label")},
-        )
+        label = { Text(text = "Label") },
+    )
 }
 
 @Composable
-private fun Example3(){
+private fun Example3() {
     val openAlertDialog = remember {
         mutableStateOf(true)
     }
 
-    when{
-        openAlertDialog.value ->{
+    when {
+        openAlertDialog.value -> {
             AlertDialog(
                 onDismissRequest = { openAlertDialog.value = false },
-                confirmButton =  {
+                confirmButton = {
                     Text(text = "Yes")
                 },
-                dismissButton =  {
+                dismissButton = {
                     Text(text = "No")
                 },
                 title = {
                     Text(text = "Are you sure?")
-                        },
+                },
                 text = {
                     Text(text = "Do you want to delete this file?")
-                       },
-                )
+                },
+            )
         }
     }
 
 
-    
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun Test2() {
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                NavigationDrawerItem(
+                    label = { Text(text = " Text 1") },
+                    selected = true,
+                    onClick = {},
+                    icon = { Icon(Icons.Filled.Call, contentDescription = null) }
+                        )
+                        Spacer(modifier = Modifier.padding(8.dp))
+                        NavigationDrawerItem(
+                            label = { Text(text = " Text 2") },
+                            selected = true,
+                            onClick = {},
+                            icon = { Icon(Icons.Filled.Email, contentDescription = null) }
+                        )
+                        Spacer(modifier = Modifier.padding(8.dp))
+                        NavigationDrawerItem(
+                            label = { Text(text = " Text 3") },
+                            selected = true,
+                            onClick = {},
+                            icon = { Icon(Icons.Filled.Place, contentDescription = null) }
+                        )
+                        Spacer(modifier = Modifier.padding(8.dp))
+                        Text(text = "Another simple text for show test!")
+                    }
+            }
+            ) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(text = "TopAppBar title")
+                        },
+                        navigationIcon = {
+                            IconButton(onClick = {}) {
+                                Icon(Icons.Filled.Menu, contentDescription = null)
+                            }
+                        }
+                    )
+                },
+                bottomBar = {
+                    NavigationBar {
+                        NavigationBarItem(
+                            selected = true,
+                            onClick = {},
+                            icon = {
+                                Icon(Icons.Outlined.Edit, contentDescription = null)
+                            },
+                            label = {
+                                Text(text = "Edit")
+                            }
+                        )
+                        NavigationBarItem(
+                            selected = true,
+                            onClick = {},
+                            icon = {
+                                Icon(Icons.Filled.Favorite, contentDescription = null)
+                            },
+                            label = {
+                                Text(text = "Favorite")
+                            }
+                        )
+                        NavigationBarItem(
+                            selected = true,
+                            onClick = {},
+                            icon = {
+                                Icon(Icons.Outlined.Delete, contentDescription = null)
+                            },
+                            label = {
+                                Text(text = "Delete")
+                            }
+                        )
+                    }
+                }
+            ) {
+                Text(
+                    modifier = Modifier.padding(it),
+                    text = "This is scaffold content",
+                )
+            }
+        }
+
+}
 
