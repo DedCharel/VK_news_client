3283ed8450f49210122bc184223c6e0ab3d7957a
Daniil Shcherbakov <Charel@bk.ru>
2025-04-23 17:11:05 +0300

    Login Screen (7.3)

--- app/src/main/java/ru/nvgsoft/vknewsclient/MainActivity.kt
+++ app/src/main/java/ru/nvgsoft/vknewsclient/MainActivity.kt
@@ -6,10 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vk.api.sdk.VK
 import com.vk.api.sdk.auth.VKAuthenticationResult
 import com.vk.api.sdk.auth.VKScope
 import ru.nvgsoft.vknewsclient.ui.theme.ActivityResultTest
+import ru.nvgsoft.vknewsclient.ui.theme.AuthState
+import ru.nvgsoft.vknewsclient.ui.theme.LoginScreen
 import ru.nvgsoft.vknewsclient.ui.theme.MainScreen
 import ru.nvgsoft.vknewsclient.ui.theme.VkNewsClientTheme
 
@@ -19,20 +23,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
+                val viewModel: MainViewModel = viewModel()
+                val authState = viewModel.authState.observeAsState(AuthState.Initial)
                 val launcher = rememberLauncherForActivityResult(
                     contract = VK.getVKAuthActivityResultContract()
                 ) {
-                    when(it){
-                        is VKAuthenticationResult.Success -> {
-                            Log.d("MainActivity", "Success auth")
-                        }
-                        is VKAuthenticationResult.Failed -> {
-                            Log.d("MainActivity", "Failed auth")
+                    viewModel.performAuthResult(it)
+                }
+
+                when(authState.value){
+                    is AuthState.Authorized -> {
+                        MainScreen()
+                    }
+                    is AuthState.NotAuthorized ->{
+                        LoginScreen {
+                            launcher.launch(listOf(VKScope.WALL))
                         }
                     }
+                    else -> {
+
+                    }
                 }
-                launcher.launch(listOf(VKScope.WALL))
-                ActivityResultTest()
+
             }
 
         }
---
+++ app/src/main/java/ru/nvgsoft/vknewsclient/ui/theme/AuthState.kt
@@ -0,0 +1,10 @@
+package ru.nvgsoft.vknewsclient.ui.theme
+
+sealed class AuthState {
+
+    object Authorized: AuthState()
+
+    object NotAuthorized: AuthState()
+
+    object Initial: AuthState()
+}
