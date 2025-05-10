c72c586e3a5bab8646ff4efd069be066e3a1a947
Daniil Shcherbakov <Charel@bk.ru>
2025-05-10 17:23:02 +0300

    Use flow instead of LiveData (9.1)

--- app/src/main/java/ru/nvgsoft/vknewsclient/data/repository/NewsFeedRepository.kt
+++ app/src/main/java/ru/nvgsoft/vknewsclient/data/repository/NewsFeedRepository.kt
@@ -4,42 +4,72 @@ import android.app.Application
 import com.vk.api.sdk.VKPreferencesKeyValueStorage
 import com.vk.api.sdk.auth.VKAccessToken
 import com.vk.id.VKID
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 import ru.nvgsoft.vknewsclient.data.mapper.NewsFeedMapper
 import ru.nvgsoft.vknewsclient.data.network.ApiFactory
 import ru.nvgsoft.vknewsclient.domain.FeedPost
 import ru.nvgsoft.vknewsclient.domain.PostComment
 import ru.nvgsoft.vknewsclient.domain.StatisticItem
 import ru.nvgsoft.vknewsclient.domain.StatisticType
+import ru.nvgsoft.vknewsclient.extensions.mergeWith
 
 class NewsFeedRepository(application: Application) {
 
-    val storage = VKPreferencesKeyValueStorage(application)
+    private val storage = VKPreferencesKeyValueStorage(application)
     val token = VKAccessToken.restore(storage)
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
+    private val nextDataNeededEvents = MutableSharedFlow<Unit>(replay = 1)
+    private val refreshedListFlow = MutableSharedFlow<List<FeedPost>>()
+    private val loadedListFlow = flow {
+        nextDataNeededEvents.emit(Unit)
+        nextDataNeededEvents.collect {
+            val startFrom = nextFrom
+            if (startFrom == null && feedPosts.isNotEmpty()) {
+                emit(feedPosts)
+                return@collect
+            }
+
+            val response = if (startFrom == null) {
+                apiService.loadPosts(getAccessToken())
+            } else {
+                apiService.loadPosts(getAccessToken(), startFrom)
+            }
+            nextFrom = response.newsFeedContent.nextFrom
+            val posts = mapper.mapResponseToPost(response)
+            _feedPosts.addAll(posts)
+            emit(feedPosts)
+        }
+    }
 
     private val apiService = ApiFactory.apiService
     private val mapper = NewsFeedMapper()
 
     private val _feedPosts = mutableListOf<FeedPost>()
-    val feedPosts: List<FeedPost>
+    private val feedPosts: List<FeedPost>
         get() = _feedPosts.toList()
 
     private var nextFrom: String? = null
 
-    suspend fun loadPosts(): List<FeedPost> {
-        val startFrom = nextFrom
-        if (startFrom == null && feedPosts.isNotEmpty()) return feedPosts
-        val response = if (startFrom == null) {
-            apiService.loadPosts(getAccessToken())
-        } else {
-            apiService.loadPosts(getAccessToken(), startFrom)
-        }
-        nextFrom = response.newsFeedContent.nextFrom
-        val posts = mapper.mapResponseToPost(response)
-        _feedPosts.addAll(posts)
-        return feedPosts
+    val posts: StateFlow<List<FeedPost>> = loadedListFlow
+        .mergeWith(refreshedListFlow)
+        .stateIn(
+            scope = coroutineScope,
+            started = SharingStarted.Lazily,
+            initialValue = feedPosts
+        )
+
+    suspend fun loadNexData() {
+        nextDataNeededEvents.emit(Unit)
     }
 
-    suspend fun getComments(feedPost: FeedPost): List<PostComment>{
+    suspend fun getComments(feedPost: FeedPost): List<PostComment> {
         val response = apiService.getComments(
             token = getAccessToken(),
             ownerId = feedPost.communityId,
@@ -61,10 +91,11 @@ class NewsFeedRepository(application: Application) {
             itemId = feedPost.id
         )
         _feedPosts.remove(feedPost)
+        refreshedListFlow.emit(feedPosts)
     }
 
     suspend fun changedLikeStatus(feedPost: FeedPost) {
-        val response = if (feedPost.isLiked){
+        val response = if (feedPost.isLiked) {
             apiService.deleteLike(
                 token = getAccessToken(),
                 ownerId = feedPost.communityId,
@@ -85,6 +116,7 @@ class NewsFeedRepository(application: Application) {
         val newPost = feedPost.copy(statistics = newStatistics, isLiked = !feedPost.isLiked)
         val postIndex = _feedPosts.indexOf(feedPost)
         _feedPosts[postIndex] = newPost
+        refreshedListFlow.emit(feedPosts)
     }
 
 
--- app/src/main/java/ru/nvgsoft/vknewsclient/presentation/news/NewsFeedViewModel.kt
+++ app/src/main/java/ru/nvgsoft/vknewsclient/presentation/news/NewsFeedViewModel.kt
@@ -1,52 +1,55 @@
 package ru.nvgsoft.vknewsclient.presentation.news
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import ru.nvgsoft.vknewsclient.data.repository.NewsFeedRepository
 import ru.nvgsoft.vknewsclient.domain.FeedPost
-import ru.nvgsoft.vknewsclient.domain.StatisticItem
+import ru.nvgsoft.vknewsclient.extensions.mergeWith
 
 class NewsFeedViewModel(application: Application) : AndroidViewModel(application) {
 
 
-
-    private val initialState = NewsFeedScreenState.Initial
-
-    private val _screenState = MutableLiveData<NewsFeedScreenState>(initialState)
-    val screenState: LiveData<NewsFeedScreenState> = _screenState
-
     private val repository = NewsFeedRepository(application)
+    private val postsFlow = repository.posts
 
-    init {
-        _screenState.value = NewsFeedScreenState.Loading
-        loadPosts()
-    }
-
-    private fun loadPosts(){
-        viewModelScope.launch {
-            val feedPosts = repository.loadPosts()
-            _screenState.value = NewsFeedScreenState.Posts(posts = feedPosts)
+    private val loadingNextDataEvent = MutableSharedFlow<Unit>()
+    private val loadNextDataFlow=   flow {
+        loadingNextDataEvent.collect{
+            emit(
+                NewsFeedScreenState.Posts(
+                    posts = postsFlow.value,
+                    nextDataIsLoading = true
+                )
+            )
         }
     }
 
+
+    val screenState = postsFlow
+        .filter { it.isNotEmpty() }
+        .map { NewsFeedScreenState.Posts(posts = it) as NewsFeedScreenState}
+        .onStart { emit(NewsFeedScreenState.Loading) }
+        .mergeWith(loadNextDataFlow)
+
+
     fun loadNextPosts(){
-        _screenState.value = NewsFeedScreenState.Posts(
-            posts = repository.feedPosts,
-            nextDataIsLoading = true
-        )
-        loadPosts()
+
+        viewModelScope.launch {
+            loadingNextDataEvent.emit(Unit)
+            repository.loadNexData()
+        }
     }
 
     fun changeLikeStatus(feedPost: FeedPost){
         viewModelScope.launch {
             repository.changedLikeStatus(feedPost)
-            _screenState.value = NewsFeedScreenState.Posts(posts = repository.feedPosts)
         }
 
     }
@@ -54,7 +57,6 @@ class NewsFeedViewModel(application: Application) : AndroidViewModel(application
     fun remove(feedPost: FeedPost){
         viewModelScope.launch {
             repository.deletePost(feedPost)
-            _screenState.value = NewsFeedScreenState.Posts(posts = repository.feedPosts)
         }
 
     }
